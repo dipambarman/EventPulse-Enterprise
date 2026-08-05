0c34809b96dfa04ab43d4ddf098e1bc860007530
Dipam Barman <dipambarman3@gmail.com>
2026-08-05 17:10:23 +0530

    fix: enforce strict CORS policy

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,8 @@ app.use(cors({
   origin: function(origin, callback){
     if(!origin) return callback(null, true);
     if(allowedOrigins.indexOf(origin) === -1){
-      return callback(null, true);
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
     }
     return callback(null, true);
   },
